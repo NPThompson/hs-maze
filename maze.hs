c3413adc1e2dc33a1c2712a7d92adc53e5bff6a4
NPThompson <noahpthomp@gmail.com>
2019-10-24 09:15:16 -0400

    explanatory comments for maze.hs

--- maze.hs
+++ maze.hs
@@ -4,24 +4,34 @@
 import System.IO
 
 
-
+--to find (x,y) index the outer list by y and then index this by x
 ix x y = (!! x) . (!! y)
 
+--the immediate neighbors of a point are the points located one unit
+--above, below, and to either side
 neighbors (x,y) = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
 
+--finds the index of the first occurence of e in a list
 firstOf e []     = 0
 firstOf e (x:xs) = if e == x then 0 else 1 + firstOf e xs
 
+--by convention, the start to the maze is the first open space on the first row,
+--the end is the first open space on the bottom row
 start m = (firstOf '.' (m !! 0), 0)
 end   m = (firstOf '.' (m !! (length m - 1)), length m - 1)
 
+--a test to determine if a point is within certain bounds
 inRect w h (x,y) = (x < w && y < h)
 notIn es = \x -> not (x `elem` es)
 
+--finds the shortest list
 minlen :: [[a]] -> [a]
 minlen [] = []
 minlen ls = foldr1 (\s0 s1 -> if length s0 > length s1 then s1 else s0) ls
 
+{-
+This implementation uses what I like to call, 'the brute force approach'. The idea is simple: at a point, test to see if it is the end point. If not, recursively search every adjacent open space, so long as that space isn't out of bounds and hasn't been visited already. Filter the results of this recursive search for the shortest path which leads to the exit. If it is the end point, or if no more paths can be found, we're done.
+-}
 solve :: [[Char]] -> [(Int,Int)]
 solve m = let e = end m
               s             = start m
@@ -38,6 +48,7 @@ solve m = let e = end m
               in
                   solve' [] s
 
+--this function returns the original maze, replacing the points in the solution path by the character 'o'
 showSoln :: [String] -> [(Int,Int)] -> [String]
 showSoln m solns = showRows m 0 where
     showRows [] n        = []
@@ -45,6 +56,7 @@ showSoln m solns = showRows m 0 where
     showColumns [] _     = []
     showColumns (c:cs) (x,y) = (if (x,y) `elem` solns then 'o' else c):(showColumns cs (x + 1, y))
 
+--main will read a maze, find the solution, and then print the solution
 main = do
     maze <- hGetContents stdin
     putStrLn $ unlines (showSoln (lines maze) (solve  $ lines maze))
